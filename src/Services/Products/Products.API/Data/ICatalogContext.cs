1b87bbe45ffd64634c15a4f3890b41105edd25fe
Tomek <tomek1989gw@wppl>
2021-05-25 17:04:31 +0200

    products api works!

---
+++ src/Services/Products/Products.API/Data/ICatalogContext.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+using Products.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Products.API.Data {
+    public interface ICatalogContext {
+
+        IMongoCollection<Product> Products { get; }
+
+    }
+}
